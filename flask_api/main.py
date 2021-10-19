a4b15cde410462b42c383010caddd3345cc0cfdd
BrimmStone127 <claybrimm@gmail.com>
2021-10-19 09:05:50 -0400

    Add vue.js frontend

---
+++ flask_api/main.py
@@ -0,0 +1,173 @@
+import flask
+from flask import request, jsonify
+from flask.helpers import flash
+import flash_controller
+import blog_controller
+import uuid
+from db import create_tables
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+BOOKS = [
+    {
+        'id': uuid.uuid4().hex,
+        'date': '01/01/1970',
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'date': '01/01/1970',
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+    },
+    {
+        'id': uuid.uuid4().hex,
+        'date': '01/01/1970',
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+    }
+]
+
+# Books route ****************************************************************
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'date': post_data.get('date'),
+            'title': post_data.get('title'),
+            'author': post_data.get('author')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'date': post_data.get('date'),
+            'title': post_data.get('title'),
+            'author': post_data.get('author')
+        })
+        response_object['message'] = 'Book updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+        return False
+
+# Blog Post Routes ***********************************************************
+
+@app.route("/blog/posts", methods=["GET"])
+def get_posts():
+    posts = blog_controller.get_posts()
+    return jsonify(posts)
+
+@app.route("/blog/post", methods=["POST"])
+def insert_post():
+    post_info = request.get_json()
+    post_date = post_info["post_date"]
+    edit_date = post_info["edit_date"]
+    location = post_info["location"] 
+    author = post_info["author"]
+    author_id = post_info["author_id"]
+    post_title = post_info["post_title"] 
+    post_body = post_info["post_body"]
+    result = blog_controller.insert_post(post_date, edit_date, location, author, author_id, post_title, post_body)
+    return jsonify(result)
+
+@app.route("/blog/post", methods=["PUT"])
+def update_post():
+    post_info = request.get_json()
+    post_id = post_info["post_id"]
+    post_date = post_info["post_date"]
+    edit_date = post_info["edit_date"]
+    location = post_info["location"] 
+    author = post_info["author"]
+    author_id = post_info["author_id"]
+    post_title = post_info["post_title"] 
+    post_body = post_info["post_body"]
+    result = blog_controller.update_post(post_id, post_date, edit_date, location, author, author_id, post_title, post_body)
+    return jsonify(result)
+
+@app.route("/blog/post/<id>", methods=["DELETE"])
+def delete_post(id):
+    result = blog_controller.delete_post(id)
+    return jsonify(result)
+
+@app.route("/blog/post/<id>", methods=["GET"])
+def get_post_by_id(id):
+    post = blog_controller.get_by_id(id)
+    return jsonify(post)
+
+# Flash Card Routes **********************************************************
+
+@app.route("/flashcards", methods=["GET"])
+def get_flashcards():
+    cards = flash_controller.get_flashcards()
+    return jsonify(cards)
+
+@app.route("/flashcard", methods=["POST"])
+def insert_flashcard():
+    card_info = request.get_json()
+    collection_id = card_info["collection_id"]
+    side_a = card_info["sideA"]
+    side_b = card_info["sideB"]
+    result = flash_controller.insert_flash(collection_id, side_a, side_b)
+    return jsonify(result)
+
+@app.route("/flashcard", methods=["PUT"])
+def update_flashcard():
+    card_info = request.get_json()
+    id = card_info["flashcard_id"]
+    collection_id = card_info["collection_id"]
+    side_a = card_info["sideA"]
+    side_b = card_info["sideB"]
+    result = flash_controller.update_flash(id, collection_id, side_a, side_b)
+    return jsonify(result)
+
+@app.route("/flashcard/<id>", methods=["DELETE"])
+def delete_flashcard(id):
+    result = flash_controller.delete_flash(id)
+    return jsonify(result)
+
+@app.route("/flashcard/<id>", methods=["GET"])
+def get_flashcard_by_id(id):
+    card = flash_controller.get_by_id(id)
+    return jsonify(card)
+    
+# sanity check route *********************************************************
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+# CORS for localhost *********************************************************
+
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
+
+if __name__ == "__main__":
+    create_tables()
+    app.run(host='127.0.0.1', port=8000, debug=False)
--- frontend/client/src/components/Books.vue
+++ frontend/client/src/components/Books.vue
@@ -4,7 +4,8 @@
       <div class="col-sm-10">
         <h1>Books</h1>
         <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Add Book</button>
+        <alert :message=message v-if="showMessage"></alert>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Book</button>
         <br><br>
         <table class="table table-hover">
           <thead>
@@ -16,14 +17,25 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>foo</td>
-              <td>bar</td>
-              <td>foobar</td>
+            <tr v-for="(book, index) in books" :key="index">
+              <td>{{book.date}}</td>
+              <td>{{book.title}}</td>
+              <td>{{book.author}}</td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
-                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                  <button
+                      type="button"
+                      class="btn btn-warning btn-sm"
+                      v-b-modal.book-update-modal
+                      @click="editBook(book)">
+                      Update
+                    </button>
+                  <button
+                      type="button"
+                      class="btn btn-danger btn-sm"
+                      @click="onDeleteBook(book)">
+                    Delete
+                  </button>
                 </div>
               </td>
             </tr>
@@ -31,5 +43,216 @@
         </table>
       </div>
     </div>
+    <b-modal ref="addBookModal"
+         id="book-modal"
+         title="Add a new book"
+         hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group id="form-date-group"
+                    label="Date:"
+                    label-for="form-date-input">
+          <b-form-input id="form-date-input"
+                        type="text"
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-title-group"
+                    label="Title:"
+                    label-for="form-title-input">
+          <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="addBookForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-group"
+                      label="Author:"
+                      label-for="form-author-input">
+            <b-form-input id="form-author-input"
+                          type="text"
+                          v-model="addBookForm.author"
+                          required
+                          placeholder="Enter author">
+            </b-form-input>
+          </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
+    <b-modal ref="editBookModal"
+         id="book-update-modal"
+         title="Update"
+         hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+        <b-form-group id="form-date-edit-group"
+                      label="Date:"
+                      label-for="form-date-edit-input">
+            <b-form-input id="form-date-edit-input"
+                          type="text"
+                          v-model="editForm.date"
+                          required
+                          placeholder="Enter date">
+            </b-form-input>
+          </b-form-group>
+        <b-form-group id="form-title-edit-group"
+                    label="Title:"
+                    label-for="form-title-edit-input">
+          <b-form-input id="form-title-edit-input"
+                        type="text"
+                        v-model="editForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-author-edit-group"
+                      label="Author:"
+                      label-for="form-author-edit-input">
+            <b-form-input id="form-author-edit-input"
+                          type="text"
+                          v-model="editForm.author"
+                          required
+                          placeholder="Enter author">
+            </b-form-input>
+          </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Update</b-button>
+          <b-button type="reset" variant="danger">Cancel</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+import Alert from './Alert.vue';
+
+export default {
+  data() {
+    return {
+      books: [],
+      addBookForm: {
+        date: '',
+        title: '',
+        author: '',
+      },
+      editForm: {
+        id: '',
+        date: '',
+        title: '',
+        author: '',
+      },
+      showMessage: false,
+    };
+  },
+  components: {
+    alert: Alert,
+  },
+  methods: {
+    getBooks() {
+      const path = 'http://localhost:8000/books';
+      axios.get(path)
+        .then((res) => {
+          this.books = res.data.books;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+            console.error(error);
+        });
+    },
+    addBook(payload) {
+      const path = 'http://localhost:8000/books';
+      axios.post(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book added!';
+          this.showMessage = true;
+        })
+        .catch(() => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getBooks();
+        });
+    },
+    updateBook(payload, bookID) {
+      const path = `http://localhost:8000/books/${bookID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    removeBook(bookID) {
+      const path = `http://localhost:8000/books/${bookID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getBooks();
+          this.message = 'Book removed!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getBooks();
+        });
+    },
+    onDeleteBook(book) {
+      this.removeBook(book.id);
+    },
+    editBook(book) {
+      this.editForm = book;
+    },
+    initForm() {
+      this.addBookForm.date = '';
+      this.addBookForm.title = '';
+      this.addBookForm.author = '';
+      this.editForm.id = '';
+      this.editForm.date = '';
+      this.editForm.title = '';
+      this.editForm.author = '';
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      const payload = {
+        date: this.addBookForm.date,
+        title: this.addBookForm.title,
+        author: this.addBookForm.author,
+      };
+      this.addBook(payload);
+      this.initForm();
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      const payload = {
+        date: this.editForm.date,
+        title: this.editForm.title,
+        author: this.editForm.author,
+      };
+      this.updateBook(payload, this.editForm.id);
+    },
+    OnResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editBookModal.hide();
+      this.initForm();
+      this.getBooks();
+    },
+    OnReset(evt) {
+      evt.preventDefault();
+      this.$refs.addBookModal.hide();
+      this.initForm();
+    },
+  },
+  created() {
+    this.getBooks();
+  },
+};
+</script>
