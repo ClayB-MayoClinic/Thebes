261cd4bcc71d863da15332204407eb0a6cbf80eb
BrimmStone127 <claybrimm@gmail.com>
2021-10-28 15:03:29 -0400

    Updated BlogView

--- spa_frontend/client/src/components/BlogView.vue
+++ spa_frontend/client/src/components/BlogView.vue
@@ -16,15 +16,38 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 import BlogList from './BlogList.vue';
 import NavBar from './NavBar.vue';
 import BlogFocus from './BlogFocus.vue';
 
 export default {
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  methods: {
+    getBlogs() {
+      const path = 'http://127.0.0.1:8000/blog/posts';
+      axios.get(path)
+        .then((res) => {
+          this.blogs = res.data.blogs;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+           console.error(error);
+        });
+    },
+  },
   components: {
     bloglist: BlogList,
     navbar: NavBar,
     blogfocus: BlogFocus,
+  },
+  created() {
+    this.getBlogs();
   },
 };
 </script>
